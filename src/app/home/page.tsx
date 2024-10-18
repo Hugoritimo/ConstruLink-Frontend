0557e9bb667f7f748d0e809960660b669d70bb28
Hugoritimo <hugoritimo@gmail.com>
2024-10-18 13:23:29 -0300

    Pagina de Gerencia

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { FaFileAlt, FaBuilding, FaBell, FaUser } from "react-icons/fa";
+import { FaBuilding, FaBell, FaUser, FaKey } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 
 const HomePage = () => {
   const router = useRouter();
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const correctPassword = "teste"; // Defina a senha correta aqui
+
+  // Função para verificar a senha
+  const handlePasswordSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password === correctPassword) {
+      setErrorMessage("");
+      setShowPasswordModal(false);
+      router.push("/gerencia/add-user"); // Redireciona para a página de criar usuário na área de gerência
+    } else {
+      setErrorMessage("Senha incorreta, tente novamente.");
+    }
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#f9f9f9] p-6">
@@ -14,7 +31,7 @@ const HomePage = () => {
       <header className="w-full max-w-5xl flex justify-between items-center mb-8">
         <h1 className="text-4xl font-extrabold text-[#af1b1b]">ConstruLink</h1>
 
-        {/* Perfil Button with Lordicon */}
+        {/* Perfil Button */}
         <Button
           variant="ghost"
           onClick={() => router.push("/profile")}
@@ -22,7 +39,6 @@ const HomePage = () => {
         >
           <FaUser className="mr-2" />
           Perfil
-          {/* Lordicon */}
         </Button>
       </header>
 
@@ -55,6 +71,22 @@ const HomePage = () => {
           </CardTitle>
           <CardContent className="text-center text-[#666666]">
             <p>Veja as últimas atualizações e notificações.</p>
+          </CardContent>
+        </Card>
+
+        {/* Botão para Área de Gerência */}
+        <Card
+          onClick={() => router.push("/gerencia/dashboard")}
+          className="cursor-pointer hover:shadow-lg transition-shadow bg-white border border-[#cccccc] rounded-lg overflow-hidden"
+        >
+          <CardHeader className="flex flex-col items-center bg-[#af1b1b] p-6">
+            <FaKey className="text-5xl text-white" />
+          </CardHeader>
+          <CardTitle className="text-center mt-4 text-xl font-semibold text-[#333333]">
+            Área de Gerência
+          </CardTitle>
+          <CardContent className="text-center text-[#666666]">
+            <p>Gerencie usuários e visualize RDOS.</p>
           </CardContent>
         </Card>
       </main>
