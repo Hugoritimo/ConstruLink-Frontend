f77ee824a46b79e8f1de61a3e72cca2ee371c6d3
Victor Hugo Sousa da Silva <hugoritimo@gmail.com>
2024-10-20 02:05:50 -0300

    Teste

--- src/pages/api/generate-pdf.ts
+++ src/pages/api/generate-pdf.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
@@ -27,33 +26,3 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(500).json({ error: 'Erro ao gerar o PDF' });
     }
 }
-=======
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { htmlContent } = req.body;
-
-    try {
-        const response = await axios.post(
-            'https://api.pdfshift.io/v3/convert/pdf',
-            { source: htmlContent },
-            {
-                headers: {
-                    Authorization: `Basic ${Buffer.from(
-                        `api:${process.env.PDFSHIFT_API_KEY}`
-                    ).toString('base64')}`,
-                    'Content-Type': 'application/json',
-                },
-                responseType: 'arraybuffer',
-            }
-        );
-
-        res.setHeader('Content-Type', 'application/pdf');
-        res.send(response.data);
-    } catch (error) {
-        console.error('Erro ao gerar o PDF:', error);
-        res.status(500).json({ error: 'Erro ao gerar o PDF' });
-    }
-}
->>>>>>> b5cf68c624c93caf3852b25a9656242eb966ac87
